d9b86fe71f23cc99234980bde545cb1b95ddd94b
daisybaicai <360619895@qq.com>
2023-06-01 21:25:54 +0800

    feat: 完成基础表单的升级

--- src/common/enum.js
+++ src/common/enum.js
@@ -33,3 +33,58 @@ export const FORM_TYPES = {
         desc: '文件'
     },
 }
+
+export const FORM_ITEM_TYPE = {
+    INPUT: {
+      code: "INPUT",
+      desc: "输入框",
+    },
+    SELECT: {
+      code: "SELECT",
+      desc: "选择框",
+    },
+    RADIO: {
+      code: "RADIO",
+      desc: "单选",
+    },
+    CHECKBOX: {
+      code: "CHECKBOX",
+      desc: "多选",
+    },
+    CHECKBOX: {
+      code: "CHECKBOX",
+      desc: "多选",
+    },
+    TEXT_AREA: {
+      code: "TEXT_AREA",
+      desc: "长文本多行输入框",
+    },
+    EMAIL: {
+      code: "EMAIL",
+      desc: "邮箱输入框",
+    },
+    DATE: {
+      code: "DATE",
+      desc: "日期输入框",
+    },
+    NUM_RANGE: {
+      code: "NUM_RANGE",
+      desc: "数值区间",
+    },
+    NUM: {
+      code: "NUM",
+      desc: "单一数值",
+    },
+    FILE: {
+      code: "FILE",
+      desc: "附件",
+    },
+    CASCADER: {
+      code: "CASCADER",
+      desc: "级联",
+    },
+    RICH_TEXT: {
+      code: "RICH_TEXT",
+      desc: "富文本",
+    },
+  };
